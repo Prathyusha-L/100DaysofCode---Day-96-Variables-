b26d7a5c52134a20a766baa0a4d750e52bf12e4b
L Prathyusha <83442712+Prathyusha-L@users.noreply.github.com>
2021-09-29 10:18:12 +0530

    Add files via upload

---
+++ Variables.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// declaration and definition
+	// of variable 'a123'
+	char a123 = 'a';
+
+	// This is also both declaration and definition
+	// as 'b' is allocated memory and
+	// assigned some garbage value.
+	float b;
+
+	// multiple declarations and definitions
+	int _c, _d45, e;
+
+	// Let us print a variable
+	cout << a123 << endl;
+
+	return 0;
+}
+
+
